7c8c01a60cb4ef25a4790c22811db482756d18b0
Fahreddin Özcan <ozcanfahrettinn@gmail.com>
2023-11-10 12:43:35 +0300

    +redis

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,17 +1,19 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 declare global {
-	namespace App {
-        interface Platform {
-          env: {
-            COUNTER: DurableObjectNamespace;
-          };
-          context: {
-            waitUntil(promise: Promise<any>): void;
-          };
-          caches: CacheStorage & { default: Cache }
-        }
+  namespace App {
+    interface Platform {
+      env: {
+        COUNTER: DurableObjectNamespace;
+        UPSTASH_REDIS_REST_URL: string;
+        UPSTASH_REDIS_REST_TOKEN: string;
+      };
+      context: {
+        waitUntil(promise: Promise<any>): void;
+      };
+      caches: CacheStorage & { default: Cache };
     }
+  }
 }
 
 export {};
